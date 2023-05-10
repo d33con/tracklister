9f608385f928d6e5efeb7b9712d705646d1cfa60
Oli Bullen <d33con@gmail.com>
2023-05-11 05:43:11 +0900

    initial project setup

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <nav>
+      <div>footer</div>
+    </nav>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <div>navbar</div>
+    </nav>
+  );
+};
+
+export default Navbar;

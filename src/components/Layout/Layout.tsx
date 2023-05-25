fffd70b22999d318f01a29e2fdcfc8e8c08c99aa
Oli Bullen <d33con@gmail.com>
2023-05-25 20:43:44 +0900

    setup font

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,17 @@
+import { Pathway_Extreme } from "next/font/google";
 import React from "react";
-import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+
+const font = Pathway_Extreme({ subsets: ["latin"] });
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <>
+    <div className={font.className}>
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,14 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import AudioPlayer from "@/components/AudioPlayer/AudioPlayer";
-import TracklistTable from "@/components/TracklistTable/TracklistTable";
-import { auth } from "@/firebase/clientApp";
-import { useAuthState } from "react-firebase-hooks/auth";
 import LoggedOutHomepage from "@/components/LoggedOut/LoggedOutHomepage";
-import { Container, Grid, GridItem } from "@chakra-ui/react";
-
-const inter = Inter({ subsets: ["latin"] });
+import { auth } from "@/firebase/clientApp";
+import { Text } from "@chakra-ui/react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Home() {
   const [user, loading, error] = useAuthState(auth);
 
   return (
     <section>
-      <Container>
-        {user ? (
-          <Grid>
-            <GridItem>{/* <AudioPlayer /> */}</GridItem>
-            <GridItem>{/* <TracklistTable /> */}</GridItem>
-          </Grid>
-        ) : (
-          <LoggedOutHomepage />
-        )}
-      </Container>
+      {user ? <Text>Logged in as {user.email}</Text> : <LoggedOutHomepage />}
     </section>
   );
 }

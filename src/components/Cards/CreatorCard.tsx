8327f9e61f9cf5c9c6ceba0c8a0fa31966ad8562
Oli Bullen <d33con@gmail.com>
2023-05-25 20:44:03 +0900

    creator card component

---
+++ src/components/Cards/CreatorCard.tsx
@@ -0,0 +1,58 @@
+import {
+  Avatar,
+  Badge,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Flex,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+
+const CreatorCard: React.FC = () => {
+  return (
+    <Card maxW="sm" p={6}>
+      <CardHeader textAlign="center" mb={4}>
+        <Avatar
+          src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+          size="xl"
+        />
+      </CardHeader>
+      <CardBody>
+        <Heading size="lg" textAlign="center" mb={4}>
+          Matthew
+        </Heading>
+        <Text textAlign="center" mb={3}>
+          2,015 followers
+        </Text>
+        <Flex justifyContent="center" mb={4}>
+          <Badge variant="outline" mr={2} colorScheme="gray">
+            DnB
+          </Badge>
+          <Badge variant="outline" mr={2} colorScheme="gray">
+            Oldskool
+          </Badge>
+          <Badge variant="outline" colorScheme="gray">
+            Techno
+          </Badge>
+        </Flex>
+      </CardBody>
+      <CardFooter justify="center">
+        <Button
+          as="a"
+          backgroundColor="blackAlpha.800"
+          color="whiteAlpha.900"
+          _hover={{ bg: "blackAlpha.900" }}
+          cursor="pointer"
+          textTransform="uppercase"
+        >
+          View Creator
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+export default CreatorCard;
--- src/components/LoggedOut/LoggedOutHomepage.tsx
+++ src/components/LoggedOut/LoggedOutHomepage.tsx
@@ -1,40 +1,14 @@
+import { HStack } from "@chakra-ui/react";
 import React from "react";
-import {
-  Container,
-  Card,
-  Button,
-  Image,
-  CardBody,
-  CardHeader,
-  Badge,
-} from "@chakra-ui/react";
+import CreatorCard from "../Cards/CreatorCard";
 
 const LoggedOutHomepage: React.FC = () => {
   return (
-    <section>
-      <Container>
-        <Card>
-          <CardHeader textAlign="center">
-            <Image
-              src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-              alt="username"
-              boxSize="50px"
-              borderRadius="full"
-            />
-          </CardHeader>
-          <CardHeader textAlign="center">Matthew</CardHeader>
-          <CardHeader textAlign="center">2,015 followers</CardHeader>
-          <CardBody textAlign="center">
-            <Badge variant="outline">DnB</Badge>
-            <Badge variant="outline">Oldskool</Badge>
-            <Badge variant="outline">Techno</Badge>
-          </CardBody>
-          <CardBody textAlign="center">
-            <Button as="a">View Creator</Button>
-          </CardBody>
-        </Card>
-      </Container>
-    </section>
+    <HStack bg="blue.300" spacing={6} justifyContent="center" pt={12} pb={12}>
+      <CreatorCard />
+      <CreatorCard />
+      <CreatorCard />
+    </HStack>
   );
 };
 

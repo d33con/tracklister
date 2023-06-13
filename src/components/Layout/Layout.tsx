5c0925c3ae17b02bd57e48ec9186cb84ea27b335
Oli Bullen <d33con@gmail.com>
2023-06-13 15:05:03 +0900

    load mixes into player on initial start

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,48 @@
+import { firestore } from "@/firebase/clientApp";
+import useMixes from "@/hooks/useMixes";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
 import { Pathway_Extreme } from "next/font/google";
-import React from "react";
+import React, { useEffect } from "react";
+import Player from "../AudioPlayer/Player";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
-import Player from "../AudioPlayer/Player";
+import { Mix } from "@/atoms/mixesAtom";
 
 const font = Pathway_Extreme({ subsets: ["latin"] });
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const {
+    mixStateValue,
+    setMixStateValue,
+    onFavouriteMix,
+    onSelectMix,
+    onDeleteMix,
+  } = useMixes();
+
+  useEffect(() => {
+    const getLatestMix = async () => {
+      try {
+        // get latest mixes on initial load
+        const mixesQuery = query(
+          collection(firestore, "mixes"),
+          orderBy("createdAt", "desc"),
+          limit(4)
+        );
+
+        const mixDocs = await getDocs(mixesQuery);
+
+        const mixes = mixDocs.docs.map((mix) => ({ ...mix.data() }));
+        setMixStateValue((prevState) => ({
+          ...prevState,
+          currentlyPlayingMix: mixes[0] as Mix,
+        }));
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    };
+    getLatestMix();
+  }, [setMixStateValue]);
+
   return (
     <div className={font.className}>
       <Navbar />

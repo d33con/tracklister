6850f6ab1a939a1774db9e338ad7bb9782fac170
Oli Bullen <d33con@gmail.com>
2023-06-14 16:29:31 +0900

    homepage styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,41 @@
 import LoggedOutHomepage from "@/components/LoggedOut/LoggedOutHomepage";
 import Mixes from "@/components/Mixes/Mixes";
 import { auth } from "@/firebase/clientApp";
-import { Text } from "@chakra-ui/react";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Home() {
   const [user, loading, error] = useAuthState(auth);
 
   return (
-    <section>
-      {user ? <Text>Logged in as {user.email}</Text> : <LoggedOutHomepage />}
-      <Mixes />
-    </section>
+    <>
+      <Flex direction="column" justifyContent="center">
+        {user ? (
+          <>
+            <Text
+              textAlign="center"
+              fontSize="18px"
+              pb={8}
+              pt={8}
+              backgroundColor="green.200"
+            >
+              Hello, {user.email}
+            </Text>
+            <Heading pl={10} pr={10} mt={4}>
+              Latest Mixes
+            </Heading>
+            <Mixes />
+          </>
+        ) : (
+          <>
+            <LoggedOutHomepage />
+            <Heading pl={10} pr={10} mt={4}>
+              Latest Mixes
+            </Heading>
+            <Mixes />
+          </>
+        )}
+      </Flex>
+    </>
   );
 }

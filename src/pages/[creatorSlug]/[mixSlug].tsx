608f9850f5338bb7f26bcfdc1cd5945774121a03
Oli Bullen <d33con@gmail.com>
2023-07-06 18:07:44 +0100

    add edit and delete buttons

--- src/pages/[creatorSlug]/[mixSlug].tsx
+++ src/pages/[creatorSlug]/[mixSlug].tsx
@@ -1,8 +1,9 @@
-import { Mix, mixState } from "@/atoms/mixesAtom";
+import { Mix } from "@/atoms/mixesAtom";
 import PageNotFound from "@/components/Error/PageNotFound";
 import { auth, firestore } from "@/firebase/clientApp";
 import useCreator from "@/hooks/useCreator";
 import useMixes from "@/hooks/useMixes";
+import useUser from "@/hooks/useUser";
 import {
   Avatar,
   Button,
@@ -25,17 +26,23 @@ import {
   Th,
   Thead,
   Tr,
+  useToast,
 } from "@chakra-ui/react";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { GetServerSidePropsContext } from "next";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { BsPlayCircle, BsSoundwave, BsCalendar2Week } from "react-icons/bs";
-import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-import formatDistanceToNow from "date-fns/formatDistanceToNow";
-import { useRecoilValue } from "recoil";
-import { User } from "firebase/auth";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import {
+  BsCalendar2Week,
+  BsPauseCircle,
+  BsPlayCircle,
+  BsSoundwave,
+} from "react-icons/bs";
+import { RiDeleteBin7Line, RiEditBoxLine } from "react-icons/ri";
 
 type MixPageProps = {
   slug: string;
@@ -46,10 +53,20 @@ const MixPage: React.FC<MixPageProps> = ({ slug, creatorSlug }) => {
   const [user] = useAuthState(auth);
   const [isLoading, setIsLoading] = useState(false);
   const [notFound, setNotFound] = useState(false);
-  const { setMixStateValue, onPlayMix, onFavouriteMix } = useMixes();
-  const mixStateValue = useRecoilValue(mixState);
+  const {
+    setMixStateValue,
+    onPlayMix,
+    onFavouriteMix,
+    onPauseMix,
+    mixStateValue,
+    onDeleteMix,
+  } = useMixes();
+  const [deletingMix, setDeletingMix] = useState(false);
 
   const { getCreatorFromSlug } = useCreator();
+  const { currentUser } = useUser();
+  const toast = useToast();
+  const router = useRouter();
 
   useEffect(() => {
     const getSelectedMix = async () => {
@@ -84,6 +101,38 @@ const MixPage: React.FC<MixPageProps> = ({ slug, creatorSlug }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [setMixStateValue, slug, creatorSlug]);
 
+  const handleDeleteMix = async () => {
+    try {
+      setDeletingMix(true);
+      const success = await onDeleteMix(mixStateValue.selectedMix!);
+      if (!success) {
+        throw new Error("Failed to delete mix");
+      }
+      // success toast
+      toast({
+        title: "Mix deleted.",
+        description: "Your mix has succesfully been deleted",
+        status: "success",
+        position: "top",
+        duration: 3000,
+        isClosable: true,
+      });
+    } catch (error: any) {
+      // fail toast
+      toast({
+        title: "Your mix could not be deletd.",
+        description: "Please try again.",
+        status: "error",
+        position: "top",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+    setDeletingMix(false);
+    // redirect back to dashboard
+    router.push("/dashboard/my-shows");
+  };
+
   return (
     <Flex direction="column">
       {isLoading ? (
@@ -98,69 +147,104 @@ const MixPage: React.FC<MixPageProps> = ({ slug, creatorSlug }) => {
             <Flex direction="row" alignItems="center" px={24}>
               <Flex direction="column">
                 <Flex direction="row" alignItems="center" mb={8}>
-                  <IconButton
-                    variant="link"
-                    aria-label="Play audio"
-                    fontSize="100px"
-                    color="whiteAlpha.900"
-                    mr={8}
-                    icon={<BsPlayCircle />}
-                    onClick={() => onPlayMix(mixStateValue.selectedMix!)}
-                  />
+                  {mixStateValue.currentlyPlayingMix?.id ===
+                    mixStateValue.selectedMix?.id &&
+                  mixStateValue.audioPlaying ? (
+                    <IconButton
+                      variant="link"
+                      aria-label="Play audio"
+                      fontSize="100px"
+                      color="whiteAlpha.900"
+                      mr={8}
+                      icon={<BsPauseCircle />}
+                      onClick={onPauseMix}
+                    />
+                  ) : (
+                    <IconButton
+                      variant="link"
+                      aria-label="Play audio"
+                      fontSize="100px"
+                      color="whiteAlpha.900"
+                      mr={8}
+                      icon={<BsPlayCircle />}
+                      onClick={() => onPlayMix(mixStateValue.selectedMix!)}
+                    />
+                  )}
                   <Heading color="whiteAlpha.900">
                     {mixStateValue.selectedMixCreator?.creatorName} -{" "}
                     {mixStateValue.selectedMix?.title}
                   </Heading>
                 </Flex>
-                <Flex
-                  direction="row"
-                  justifyContent="space-between"
-                  width="100%"
-                >
-                  <Button
-                    leftIcon={
-                      mixStateValue.selectedMix?.favouritedByUsers?.includes(
-                        user?.uid as string
-                      ) ? (
-                        <AiFillHeart />
-                      ) : (
-                        <AiOutlineHeart />
-                      )
-                    }
-                    color={
-                      mixStateValue.selectedMix?.favouritedByUsers?.includes(
-                        user?.uid as string
-                      )
-                        ? "blackAlpha.700"
-                        : "whiteAlpha.700"
-                    }
-                    backgroundColor={
-                      mixStateValue.selectedMix?.favouritedByUsers?.includes(
-                        user?.uid as string
-                      )
-                        ? "whiteAlpha.800"
-                        : "none"
-                    }
-                    _hover={{
-                      color:
-                        mixStateValue.selectedMix?.favouritedByUsers?.includes(
-                          user?.uid as string
-                        )
-                          ? "blackAlpha.600"
-                          : "whiteAlpha.600",
-                    }}
-                    variant={"outline"}
-                    size="sm"
-                    mr={2}
-                    onClick={() =>
-                      onFavouriteMix(
-                        mixStateValue.selectedMix as Mix,
-                        user as User
-                      )
-                    }
-                  >
-                    {mixStateValue.selectedMix?.favouriteCount}
-                  </Button>
+                <Flex direction="row" width="100%">
+                  {mixStateValue.selectedMix?.favouritedByUsers?.includes(
+                    user?.uid as string
+                  ) ? (
+                    <Button
+                      leftIcon={<AiFillHeart />}
+                      color="blackAlpha.700"
+                      backgroundColor="whiteAlpha.800"
+                      _hover={{
+                        color: "blackAlpha.600",
+                      }}
+                      variant="outline"
+                      size="sm"
+                      mr={8}
+                      onClick={() =>
+                        onFavouriteMix(mixStateValue.selectedMix as Mix)
+                      }
+                    >
+                      {mixStateValue.selectedMix?.favouriteCount}
+                    </Button>
+                  ) : (
+                    <Button
+                      leftIcon={<AiOutlineHeart />}
+                      color="whiteAlpha.700"
+                      backgroundColor="none"
+                      _hover={{
+                        color: "whiteAlpha.600",
+                      }}
+                      variant={"outline"}
+                      size="sm"
+                      mr={8}
+                      onClick={() =>
+                        onFavouriteMix(mixStateValue.selectedMix as Mix)
+                      }
+                    >
+                      {mixStateValue.selectedMix?.favouriteCount}
+                    </Button>
+                  )}
+                  {user &&
+                    currentUser?.uid ===
+                      mixStateValue.selectedMix?.creatorId && (
+                      <>
+                        <Link
+                          as={NextLink}
+                          href={`/${mixStateValue.selectedMix?.creatorSlug}/${mixStateValue.selectedMix?.slug}/edit`}
+                        >
+                          <Button
+                            leftIcon={<RiEditBoxLine />}
+                            color="blackAlpha.900"
+                            variant="outline"
+                            size="sm"
+                            mr={2}
+                          >
+                            Edit
+                          </Button>
+                        </Link>
+                        <Button
+                          leftIcon={<RiDeleteBin7Line />}
+                          color="blackAlpha.900"
+                          variant="outline"
+                          size="sm"
+                          mr={2}
+                          onClick={handleDeleteMix}
+                          isLoading={deletingMix}
+                        >
+                          Delete
+                        </Button>
+                      </>
+                    )}
+                  <Spacer />
                   <Flex alignItems="center">
                     <Icon
                       as={BsCalendar2Week}
